b42ada36ce448c2840ccb0950fcdf62c91eede94
Marques960 <rafaeltavaresmarques06@gmail.com>
2024-08-01 08:56:53 +0100

    pagina main.dart finalizada / criação pagina para inseri a função do excel

--- excel/lib/main.dart
+++ excel/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,11 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -34,15 +35,53 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          "Flutter Excel"
+        backgroundColor: Colors.blueGrey,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 15),
+          child: Text(
+            "Flutter Excel",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           children: [
-            
+            SizedBox(height: 500),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: 200,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.blue
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Center(
+                      child: Text(
+                        "Download Excel",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.download,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
